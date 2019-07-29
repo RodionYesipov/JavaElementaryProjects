6587f91a39154a85d2b4ada28a4fc36dbd495509
RodionYesipov <rodion.yesipov@gmail.com>
2019-07-29 21:56:06 +0300

    task1

---
+++ JE_lesson8/src/HomeTask1.java
@@ -0,0 +1,29 @@
+/**
+* 1. Число Фибоначи
+ * написать метод который возвращает N-ное число фибоначи, по правилу:
+ * F(0) = 0, F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2)
+* */
+
+public class HomeTask1 {
+    private static int fibonachiCall(int index){
+        if(index <= 0){
+            return 0;
+        } else if(index == 1){
+            return 1;
+        } else if(index == 2){
+            return 1;
+        } else {
+            return fibonachiCall(index - 1) + fibonachiCall(index - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int endIndex = 10;
+        System.out.println("Fibonachi series up to index " + endIndex);
+        for (int i = 0; i <= endIndex; i++) {
+            System.out.print(fibonachiCall(i) + " ");
+        }
+
+    }
+}
